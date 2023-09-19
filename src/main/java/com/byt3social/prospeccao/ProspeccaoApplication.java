b38fc803f9ce87b3611e963632018bf55c4c1e62
Leandro Pereira <leojgpereira@gmail.com>
2023-09-19 19:32:39 -0400

    Commit inicial

---
+++ src/main/java/com/byt3social/prospeccao/ProspeccaoApplication.java
@@ -0,0 +1,13 @@
+package com.byt3social.prospeccao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspeccaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspeccaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byt3social/prospeccao/ProspeccaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byt3social.prospeccao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProspeccaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
